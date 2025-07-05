0c0814d24641462e51cc079b47f544dbea09ae92
SwarnavaPanda <swarnavapanda@gmail.com>
2025-07-05 22:24:10 +0530

    Initial commit: GitHub Webhook Dashboard with Flask & MongoDB

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from pymongo import MongoClient
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+import pytz
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+# MongoDB Connection
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["webhook_db"]
+collection = db["events"]
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    event_type = request.headers.get('X-GitHub-Event')
+    payload = request.json
+
+    ist = pytz.timezone('Asia/Kolkata')
+    timestamp = datetime.now(ist).strftime("%d %B %Y - %I:%M %p IST")
+    entry = {}
+
+    if event_type == "push":
+        entry = {
+            "author": payload["pusher"]["name"],
+            "action_type": "push",
+            "to_branch": payload["ref"].split("/")[-1],
+            "timestamp": timestamp
+        }
+
+    elif event_type == "pull_request":
+        pr = payload["pull_request"]
+        author = pr["user"]["login"]
+        from_branch = pr["head"]["ref"]
+        to_branch = pr["base"]["ref"]
+        merged = pr.get("merged", False)
+        action = payload.get("action")
+
+        if action == "opened":
+            entry = {
+                "author": author,
+                "action_type": "pull_request",
+                "from_branch": from_branch,
+                "to_branch": to_branch,
+                "timestamp": timestamp
+            }
+        elif action == "closed" and merged:
+            entry = {
+                "author": author,
+                "action_type": "merge",
+                "from_branch": from_branch,
+                "to_branch": to_branch,
+                "timestamp": timestamp
+            }
+
+    if entry:
+        collection.insert_one(entry)
+
+    return '', 200
+
+@app.route('/events', methods=['GET'])
+def get_events():
+    events = list(collection.find({}, {'_id': 0}))
+    return jsonify(events)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
